85f0ac495a59099d5958230208f6b00aa32f06e9
liyongshuai <liys0820@163.com>
2025-07-02 22:05:09 +0800

    准备发布 v1.0.2

--- sysinfo.rb
+++ sysinfo.rb
@@ -1,7 +1,7 @@
 class Sysinfo < Formula
   desc "A comprehensive system and network information tool"
   homepage "https://github.com/SKT-Shurima/homebrew-sysinfo"
-  url "https://github.com/SKT-Shurima/homebrew-sysinfo/archive/refs/tags/v1.0.1.tar.gz"
+  url "https://github.com/SKT-Shurima/homebrew-sysinfo/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "1daa6c7b2d142d5f32fd33e04e5e9815082550df959fedc5e1d742d056bb14d4"
   license "MIT"
   head "https://github.com/SKT-Shurima/homebrew-sysinfo.git", branch: "main"
